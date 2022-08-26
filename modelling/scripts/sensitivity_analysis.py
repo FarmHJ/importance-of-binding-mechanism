be6a110b3d062c6d34390e26319d6cfcc69d91d5
FarmHJ <huijia-farm_95@hotmail.com>
2022-08-26 15:07:17 +0100

    Debugging simulation error

--- modelling/scripts/sensitivity_analysis.py
+++ modelling/scripts/sensitivity_analysis.py
@@ -56,59 +56,59 @@ drug_model.current_head = drug_model.model.get('ikr')
 cmap = matplotlib.cm.get_cmap('viridis')
 norm = matplotlib.colors.Normalize(0, len(drug_conc))
 
-peak_combine = []
-model_name = []
-for p in range(len(protocols)):
-    drug_model.protocol = protocols[p]
+# peak_combine = []
+# model_name = []
+# for p in range(len(protocols)):
+#     drug_model.protocol = protocols[p]
 
-    # Simulate hERG current
-    peaks = []
+#     # Simulate hERG current
+#     peaks = []
 
-    for i in range(len(drug_conc)):
-        log = drug_model.drug_simulation(drug, drug_conc[i], repeats)
-        peak, _ = drug_model.extract_peak(log, 'ikr.IKr')
-        peaks.append(peak[-1])
+#     for i in range(len(drug_conc)):
+#         log = drug_model.drug_simulation(drug, drug_conc[i], repeats)
+#         peak, _ = drug_model.extract_peak(log, 'ikr.IKr')
+#         peaks.append(peak[-1])
 
-    peak_combine.append(peaks)
+#     peak_combine.append(peaks)
 
-Hill_coef_df = pd.DataFrame(columns=['Hill coefficient', 'IC50', 'protocol'])
+# Hill_coef_df = pd.DataFrame(columns=['Hill coefficient', 'IC50', 'protocol'])
 
-for i in range(len(peak_combine)):
-    peaks = peak_combine[i]
-    peaks = (peaks - min(peaks)) / (max(peaks) - min(peaks))
-    estimates, _ = optimiser.optimise(drug_conc, peaks)
+# for i in range(len(peak_combine)):
+#     peaks = peak_combine[i]
+#     peaks = (peaks - min(peaks)) / (max(peaks) - min(peaks))
+#     estimates, _ = optimiser.optimise(drug_conc, peaks)
 
-    Hill_df = pd.DataFrame({'Hill coefficient': [estimates[0]],
-                            'IC50': [estimates[1]],
-                            'protocol': [protocol_list[i]]})
-    Hill_coef_df = pd.concat([Hill_coef_df, Hill_df])
+#     Hill_df = pd.DataFrame({'Hill coefficient': [estimates[0]],
+#                             'IC50': [estimates[1]],
+#                             'protocol': [protocol_list[i]]})
+#     Hill_coef_df = pd.concat([Hill_coef_df, Hill_df])
 
-Hill_coef_df.to_csv(saved_data_dir + 'Hill_curves.csv')
+# Hill_coef_df.to_csv(saved_data_dir + 'Hill_curves.csv')
 
 # If Hill curves are computed previously
-# Hill_coef_df = pd.read_csv(saved_data_dir + 'Hill_curves.csv',
-#                            usecols=['Hill coefficient', 'IC50', 'protocol'])
+Hill_coef_df = pd.read_csv(saved_data_dir + 'Hill_curves.csv',
+                           usecols=['Hill coefficient', 'IC50', 'protocol'])
 
 # Simulate and save hERG current under stimulant of Milnes protocol
 Hill_eq = Hill_coef_df.loc[Hill_coef_df['protocol'] == protocol_list[0]]
 Hill_eq = Hill_eq.values.tolist()[0][:-1]
 
-save_signal = 2
-drug_model.protocol = protocols[0]
-for i, conc in enumerate(drug_conc):
-    log = drug_model.drug_simulation(
-        drug, conc, repeats,
-        log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
-    log.save_csv(saved_data_dir + protocol_list[0] + '/CiPA_hERG_' +
-                 str(conc) + '.csv')
+# save_signal = 2
+# drug_model.protocol = protocols[0]
+# for i, conc in enumerate(drug_conc):
+#     log = drug_model.drug_simulation(
+#         drug, conc, repeats,
+#         log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+#     log.save_csv(saved_data_dir + protocol_list[0] + '/CiPA_hERG_' +
+#                  str(conc) + '.csv')
 
-    reduction_scale = Hill_model.simulate(Hill_eq, conc)
+#     reduction_scale = Hill_model.simulate(Hill_eq, conc)
 
-    log = drug_model.conductance_simulation(
-        base_conductance * reduction_scale, repeats, timestep=0.1,
-        log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
-    log.save_csv(saved_data_dir + protocol_list[0] + '/conductance_hERG_' +
-                 str(conc) + '.csv')
+#     log = drug_model.conductance_simulation(
+#         base_conductance * reduction_scale, repeats, timestep=0.1,
+#         log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+#     log.save_csv(saved_data_dir + protocol_list[0] + '/conductance_hERG_' +
+#                  str(conc) + '.csv')
 
 # Set AP model
 APmodel = '../../model/ohara-cipa-v1-2017.mmt'
@@ -122,31 +122,36 @@ base_conductance = APmodel.get('ikr.gKr').value()
 offset = 50
 
 # Transient phase
-repeats = 10
-save_signal = repeats
+# repeats = 10
+# save_signal = repeats
 
-AP_conductance = []
-AP_trapping = []
+# AP_conductance = []
+# AP_trapping = []
 
-for i, conc in enumerate(drug_conc):
-    log = AP_model.drug_simulation(
-        drug, conc, repeats, timestep=0.1, save_signal=save_signal,
-        log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
-    log.save_csv(saved_data_dir + protocol_list[0] +
-                 '/CiPA_AP_transient_pulses' + str(repeats) +
-                 '_' + str(conc) + '.csv')
+# for i, conc in enumerate(drug_conc):
+#     log = AP_model.drug_simulation(
+#         drug, conc, repeats, timestep=0.1, save_signal=save_signal,
+#         log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+#     log.save_csv(saved_data_dir + protocol_list[0] +
+#                  '/CiPA_AP_transient_pulses' + str(repeats) +
+#                  '_' + str(conc) + '.csv')
 
-    reduction_scale = Hill_model.simulate(Hill_eq, conc)
-    d2 = AP_model.conductance_simulation(
-        base_conductance * reduction_scale, repeats, timestep=0.1,
-        save_signal=save_signal,
-        log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
-    d2.save_csv(saved_data_dir + protocol_list[0] +
-                '/conductance_AP_transient_pulses' +
-                str(repeats) + '_' + str(conc) + '.csv')
+#     reduction_scale = Hill_model.simulate(Hill_eq, conc)
+#     d2 = AP_model.conductance_simulation(
+#         base_conductance * reduction_scale, repeats, timestep=0.1,
+#         save_signal=save_signal,
+#         log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+#     d2.save_csv(saved_data_dir + protocol_list[0] +
+#                 '/conductance_AP_transient_pulses' +
+#                 str(repeats) + '_' + str(conc) + '.csv')
 
 repeats = 1000
 save_signal = 2
+
+####################
+# Only for dofetilide
+if drug == 'dofetilide' or 'ranolazine' or 'cisapride':
+    drug_conc = drug_conc[:-1]
 
 for i, conc in enumerate(drug_conc):
     log = AP_model.drug_simulation(
@@ -158,10 +163,16 @@ for i, conc in enumerate(drug_conc):
 
     reduction_scale = Hill_model.simulate(Hill_eq, conc)
 
-    d2 = AP_model.conductance_simulation(
-        base_conductance * reduction_scale, repeats, timestep=0.1,
-        save_signal=save_signal,
-        log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+    if drug == 'ranolazine' and conc >= 1e6:
+        d2 = AP_model.conductance_simulation(
+            base_conductance * reduction_scale, repeats, timestep=0.1,
+            save_signal=save_signal, abs_tol=1e-6, rel_tol=1e-5,
+            log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
+    else:
+        d2 = AP_model.conductance_simulation(
+            base_conductance * reduction_scale, repeats, timestep=0.1,
+            save_signal=save_signal,
+            log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
 
     d2.save_csv(saved_data_dir + protocol_list[0] + '/conductance_AP_' +
                 str(drug_conc[i]) + '.csv')
@@ -170,15 +181,9 @@ save_signal = repeats
 APD_conductance = []
 APD_trapping = []
 # No need to run simulation when drug concentration is zero
-drug_conc = drug_conc[1:]
-
-####################
-# Only for dofetilide
-if drug == 'dofetilide':
-    drug_conc = drug_conc[:-1]
+drug_conc = drug_conc_lib.drug_concentrations[drug]['fine']
 
 for i, conc in enumerate(drug_conc):
-    print(conc)
     log = AP_model.drug_simulation(
         drug, conc, repeats, timestep=0.1, save_signal=save_signal,
         log_var=['engine.time', 'membrane.V'])

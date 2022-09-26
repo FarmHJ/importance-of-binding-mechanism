205ccb730456e8f6adccfdbeccd50fb7e2f9e1e3
FarmHJ <huijia-farm_95@hotmail.com>
2022-09-26 20:43:31 +0100

    Check that the parameter inputs are correct

--- modelling/scripts/peak_reduction_check.py
+++ modelling/scripts/peak_reduction_check.py
@@ -1,3 +1,4 @@
+## Not complete
 # Simple checking if simulated current matches literature
 # In particular, check if the input parameters are correct
 
@@ -15,7 +16,8 @@ protocol_params = modelling.ProtocolParameters()
 pulse_time = protocol_params.protocol_parameters[protocol_name]['pulse_time']
 protocol = protocol_params.protocol_parameters[protocol_name]['function']
 repeats = 10
-drug_conc = modelling.DrugConcentrations().drug_concentrations[drug]['lit_default']
+drug_conc = modelling.DrugConcentrations().drug_concentrations[
+    drug]['lit_default']
 drug_labels = [str(i) + ' nM' for i in drug_conc]
 
 testing_fig_dir = '../../figures/testing/'
@@ -35,12 +37,14 @@ total_log = []
 peaks = []
 for i in range(len(drug_conc)):
     log = drug_model.drug_simulation(
-        drug, drug_conc[i], repeats,
+        drug, drug_conc[i], repeats, save_signal=repeats,
         log_var=['engine.time', 'membrane.V', 'ikr.IKr'])
     peak, _ = drug_model.extract_peak(log, 'ikr.IKr')
-    print(peak)
-    peaks.append(peak[-1])
-    total_log.append(log)
+    print(log['ikr.IKr', 9])
+    for j in range(repeats):
+        log['membrane.V', j]
+    # peaks.append(peak[-1])
+    # total_log.append(log)
 
 # Plot drug response against drug concentration curve
 # peaks = (peaks - min(peaks)) / (max(peaks) - min(peaks))

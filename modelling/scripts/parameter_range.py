d40fd3d01e0f17fa08a7e018b35acc8d95aafa61
FarmHJ <huijia-farm_95@hotmail.com>
2022-09-28 17:28:44 +0100

    Color code drugs for the parameter value plot

--- modelling/scripts/parameter_range.py
+++ modelling/scripts/parameter_range.py
@@ -3,6 +3,7 @@
 
 import csv
 import itertools
+import matplotlib
 import matplotlib.pyplot as plt
 import myokit
 import numpy as np
@@ -37,13 +38,24 @@ for drug in drug_list:
 
 all_params = [Vhalf, Kmax, Ku, N, EC50]
 labels = ['Vhalf', 'Kmax', 'Ku', 'N', 'EC50']
-param_ranges = [[-150, -50], [1e2, 1e6], [5e-3, 7e-2], [0.8, 1.2], [1e3, 1e8]]
+param_ranges = [[-150, -50], [30, 1e6], [5e-3, 7e-2], [0.8, 1.2], [1e3, 1e8]]
 
-# Plot of categorisation of parameters
+d = {}
+for i, param_name in enumerate(labels):
+    d[param_name] = param_ranges[i]
+
+param_cat_df = pd.DataFrame(data=d, index=['low_border', 'high_border'])
+
+# # Plot of categorisation of parameters
+# # Set up colormap
+# cmap = matplotlib.cm.get_cmap('Set1')
+# color_list = [cmap(i) for i in range(9)] + [cmap(i) for i in range(3)]
+# repeating_cmap = matplotlib.colors.ListedColormap(color_list)
+
 # nrow = 2
 # fig = modelling.figures.FigureStructure(
-#     figsize=(10, 2.5 * nrow),
-#     gridspec=(nrow, 3), hspace=0.3,
+#     figsize=(10, 2.7 * nrow),
+#     gridspec=(nrow, 3), hspace=0.7,
 #     wspace=0.2,
 #     height_ratios=[1] * nrow)
 
@@ -55,14 +67,18 @@ param_ranges = [[-150, -50], [1e2, 1e6], [5e-3, 7e-2], [0.8, 1.2], [1e3, 1e8]]
 #         param_ranges[i][1], xmin=0, xmax=len(all_params[i]),
 #         c='red')
 #     fig.axs[int(i / 3)][i % 3].scatter(
-#         np.arange(len(all_params[i])), all_params[i])
+#         np.arange(len(all_params[i])), all_params[i],
+#         c=np.arange(len(drug_list)), cmap=repeating_cmap)
 #     fig.axs[int(i / 3)][i % 3].set_title(label)
+#     fig.axs[int(i / 3)][i % 3].set_xticks(np.arange(12), labels=drug_list)
+#     plt.setp(fig.axs[int(i / 3)][i % 3].get_xticklabels(), rotation=45,
+#              ha='right', rotation_mode='anchor')
 
 # fig.axs[0][1].set_yscale("log", nonpositive='clip')
 # fig.axs[0][2].set_yscale("log", nonpositive='clip')
 # fig.axs[1][1].set_yscale("log", nonpositive='clip')
 
-# fig.savefig(saved_fig_dir + 'parameter_categorisation.pdf')
+# fig.savefig(saved_fig_dir + 'parameter_categorisation.svg', format='svg')
 
 # Simple SA - taking mean value of each category
 # Split parameter value ranges to three categories: low, medium and high
@@ -75,121 +91,131 @@ for i, param in enumerate(all_params):
     category_means.append([np.mean(low_cat), np.mean(med_cat),
                            np.mean(high_cat)])
 
-# Load IKr model
-model = '../../model/ohara-cipa-v1-2017-IKr.mmt'
-model, _, x = myokit.load(model)
+d = {}
+for i, param_name in enumerate(labels):
+    d[param_name] = category_means[i]
+param_cat_mean_df = pd.DataFrame(
+    data=d, index=['low_gp_mean', 'mid_gp_mean', 'high_gp_mean'])
 
-protocol_params = modelling.ProtocolParameters()
-pulse_time = protocol_params.protocol_parameters['Milnes']['pulse_time']
-protocol = protocol_params.protocol_parameters['Milnes']['function']
+param_cat_df = pd.concat([param_cat_df, param_cat_mean_df])
 
-drug_model = modelling.BindingKinetics(model)
-drug_model.protocol = protocol
+# param_cat_df.to_csv(saved_data_dir + 'parameter_categorisation.csv')
 
-drug_conc_Hill = list(np.append(0, 10**np.linspace(-1, 10, 12)))
+# # Load IKr model
+# model = '../../model/ohara-cipa-v1-2017-IKr.mmt'
+# model, _, x = myokit.load(model)
 
-# Set AP model
-APmodel = '../../model/ohara-cipa-v1-2017.mmt'
+# protocol_params = modelling.ProtocolParameters()
+# pulse_time = protocol_params.protocol_parameters['Milnes']['pulse_time']
+# protocol = protocol_params.protocol_parameters['Milnes']['function']
 
-# Load model
-APmodel, _, x = myokit.load(APmodel)
+# drug_model = modelling.BindingKinetics(model)
+# drug_model.protocol = protocol
 
-AP_model = modelling.BindingKinetics(APmodel, current_head='ikr')
-pulse_time = 1000
-AP_model.protocol = modelling.ProtocolLibrary().current_impulse(pulse_time)
-base_conductance = APmodel.get('ikr.gKr').value()
+# drug_conc_Hill = list(np.append(0, 10**np.linspace(-1, 10, 12)))
 
-offset = 50
-repeats_AP = 800
-save_signal = 2
+# # Set AP model
+# APmodel = '../../model/ohara-cipa-v1-2017.mmt'
 
-drug_conc_AP = 10**np.linspace(-1, 1, 3)
-# drug_conc = 10**np.linspace(-1, 7, 20)
+# # Load model
+# APmodel, _, x = myokit.load(APmodel)
 
-# Run simulation
-repeats = 1000
-APD_dict = {}
+# AP_model = modelling.BindingKinetics(APmodel, current_head='ikr')
+# pulse_time = 1000
+# AP_model.protocol = modelling.ProtocolLibrary().current_impulse(pulse_time)
+# base_conductance = APmodel.get('ikr.gKr').value()
 
-for num, param_comb in enumerate(itertools.product(*category_means)):
-    param_values = pd.DataFrame(param_comb, index=labels)
-    param_values = param_values.T
+# offset = 50
+# repeats_AP = 800
+# save_signal = 2
 
-    peaks = []
-    for i in range(len(drug_conc_Hill)):
-        log = drug_model.custom_simulation(
-            param_values, drug_conc_Hill[i], repeats,
-            log_var=['engine.time', 'ikr.IKr'])
-        peak, _ = drug_model.extract_peak(log, 'ikr.IKr')
-        peaks.append(peak[-1])
+# drug_conc_AP = 10**np.linspace(-1, 1, 3)
+# # drug_conc = 10**np.linspace(-1, 7, 20)
 
-    peaks = (peaks - min(peaks)) / (max(peaks) - min(peaks))
+# # Run simulation
+# repeats = 1000
+# APD_dict = {}
 
-    # Fit Hill curve
-    Hill_model = modelling.HillsModel()
-    optimiser = modelling.HillsModelOpt(Hill_model)
-    Hill_curve, _ = optimiser.optimise(drug_conc_Hill, peaks)
+# for num, param_comb in enumerate(itertools.product(*category_means)):
+#     param_values = pd.DataFrame(param_comb, index=labels)
+#     param_values = param_values.T
 
-    if check_plot:
-        max_grid = np.ceil(np.log(drug_conc_Hill[-1]))
-        conc_grid = np.arange(-2, max_grid, 1)
+#     peaks = []
+#     for i in range(len(drug_conc_Hill)):
+#         log = drug_model.custom_simulation(
+#             param_values, drug_conc_Hill[i], repeats,
+#             log_var=['engine.time', 'ikr.IKr'])
+#         peak, _ = drug_model.extract_peak(log, 'ikr.IKr')
+#         peaks.append(peak[-1])
 
-        plt.figure(figsize=(4, 3))
-        plt.plot(np.log(drug_conc_Hill[1:]), peaks[1:], 'o', label='peak current')
-        plt.plot(conc_grid, Hill_model.simulate(Hill_curve[:2], np.exp(conc_grid)),
-                'k', label='fitted Hill eq')
-        plt.xlabel('Drug concentration (log)')
-        plt.ylabel('Normalised peak current')
-        plt.tight_layout()
-        plt.savefig(saved_fig_dir + "Hill_curve_" + str(num) + ".pdf")
+#     peaks = (peaks - min(peaks)) / (max(peaks) - min(peaks))
 
-    data_dict = {'drug_conc_Hill': drug_conc_Hill, 'peak_current': peaks,
-                 'Hill_curve': Hill_curve[:2]}
+#     # Fit Hill curve
+#     Hill_model = modelling.HillsModel()
+#     optimiser = modelling.HillsModelOpt(Hill_model)
+#     Hill_curve, _ = optimiser.optimise(drug_conc_Hill, peaks)
 
-    APD_trapping = []
-    APD_conductance = []
-    for i in range(len(drug_conc_AP)):
-        # Run simulation for trapping model
-        log = AP_model.custom_simulation(
-            param_values, drug_conc_AP[i], repeats_AP, timestep=0.1,
-            save_signal=save_signal,
-            log_var=['engine.time', 'membrane.V'])
+#     if check_plot:
+#         max_grid = np.ceil(np.log(drug_conc_Hill[-1]))
+#         conc_grid = np.arange(-2, max_grid, 1)
 
-        # Compute APD90
-        APD_trapping_pulse = []
-        for pulse in range(save_signal):
-            apd90 = AP_model.APD90(log['membrane.V', pulse], offset, 0.1)
-            APD_trapping_pulse.append(apd90)
-        APD_trapping.append(APD_trapping_pulse)
+#         plt.figure(figsize=(4, 3))
+#         plt.plot(np.log(drug_conc_Hill[1:]), peaks[1:], 'o', label='peak current')
+#         plt.plot(conc_grid, Hill_model.simulate(Hill_curve[:2], np.exp(conc_grid)),
+#                 'k', label='fitted Hill eq')
+#         plt.xlabel('Drug concentration (log)')
+#         plt.ylabel('Normalised peak current')
+#         plt.tight_layout()
+#         plt.savefig(saved_fig_dir + "Hill_curve_" + str(num) + ".pdf")
 
-        # Run simulation for conductance model
-        reduction_scale = Hill_model.simulate(Hill_curve[:2], drug_conc_AP[i])
-        d2 = AP_model.conductance_simulation(
-            base_conductance * reduction_scale, repeats_AP, timestep=0.1,
-            save_signal=save_signal, abs_tol=1e-6, rel_tol=1e-5,
-            log_var=['engine.time', 'membrane.V'])
+#     data_dict = {'drug_conc_Hill': drug_conc_Hill, 'peak_current': peaks,
+#                  'Hill_curve': Hill_curve[:2]}
 
-        # Compute APD90
-        APD_conductance_pulse = []
-        for pulse in range(save_signal):
-            apd90 = AP_model.APD90(d2['membrane.V', pulse], offset, 0.1)
-            APD_conductance_pulse.append(apd90)
-        APD_conductance.append(APD_conductance_pulse)
+#     APD_trapping = []
+#     APD_conductance = []
+#     for i in range(len(drug_conc_AP)):
+#         # Run simulation for trapping model
+#         log = AP_model.custom_simulation(
+#             param_values, drug_conc_AP[i], repeats_AP, timestep=0.1,
+#             save_signal=save_signal,
+#             log_var=['engine.time', 'membrane.V'])
 
-    APD_trapping = [max(i) for i in APD_trapping]
-    APD_conductance = [max(i) for i in APD_conductance]
+#         # Compute APD90
+#         APD_trapping_pulse = []
+#         for pulse in range(save_signal):
+#             apd90 = AP_model.APD90(log['membrane.V', pulse], offset, 0.1)
+#             APD_trapping_pulse.append(apd90)
+#         APD_trapping.append(APD_trapping_pulse)
 
-    MSError = np.sum((np.array(APD_trapping) - np.array(APD_conductance))**2) \
-        / len(APD_trapping)
+#         # Run simulation for conductance model
+#         reduction_scale = Hill_model.simulate(Hill_curve[:2], drug_conc_AP[i])
+#         d2 = AP_model.conductance_simulation(
+#             base_conductance * reduction_scale, repeats_AP, timestep=0.1,
+#             save_signal=save_signal, abs_tol=1e-6, rel_tol=1e-5,
+#             log_var=['engine.time', 'membrane.V'])
 
-    APD_result = {**data_dict, 'param_seq': labels,
-                  'param_values': param_values.values[0],
-                  'drug_conc_AP': drug_conc_AP, 'APD_trapping': APD_trapping,
-                  'APD_conductance': APD_conductance, 'MSE': MSError}
+#         # Compute APD90
+#         APD_conductance_pulse = []
+#         for pulse in range(save_signal):
+#             apd90 = AP_model.APD90(d2['membrane.V', pulse], offset, 0.1)
+#             APD_conductance_pulse.append(apd90)
+#         APD_conductance.append(APD_conductance_pulse)
 
-    APD_dict[num] = APD_result
+#     APD_trapping = [max(i) for i in APD_trapping]
+#     APD_conductance = [max(i) for i in APD_conductance]
 
-    print(APD_dict)
+#     MSError = np.sum((np.array(APD_trapping) - np.array(APD_conductance))**2) \
+#         / len(APD_trapping)
 
-w = csv.writer(open(saved_data_dir + 'SA_param_categories.csv', 'w'))
-for key, val in APD_dict.items():
-    w.writerow([key, val])
+#     APD_result = {**data_dict, 'param_seq': labels,
+#                   'param_values': param_values.values[0],
+#                   'drug_conc_AP': drug_conc_AP, 'APD_trapping': APD_trapping,
+#                   'APD_conductance': APD_conductance, 'MSE': MSError}
+
+#     APD_dict[num] = APD_result
+
+#     print(APD_dict)
+
+# w = csv.writer(open(saved_data_dir + 'SA_param_categories.csv', 'w'))
+# for key, val in APD_dict.items():
+#     w.writerow([key, val])

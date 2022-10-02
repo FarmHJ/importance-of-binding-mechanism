3cfa7b893771504b31c794929b747294765aee58
FarmHJ <huijia-farm_95@hotmail.com>
2022-10-02 22:58:16 +0100

    Finalise global sensitivity script after testings

--- global_sensitivity.py
+++ global_sensitivity.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from SALib.sample import saltelli
 from SALib.analyze import sobol
-import matplotlib.pyplot as plt
 import myokit
 import numpy as np
 import os
@@ -17,12 +16,6 @@ saved_data_filepath = os.path.join(data_dir, 'sensitivity_analysis')
 
 if not os.path.exists(saved_data_filepath):
     os.makedirs(saved_data_filepath)
-
-fig_dir = os.path.join(os.path.dirname(__file__), 'figures')
-saved_fig_filepath = os.path.join(fig_dir, 'sensitivity_analysis')
-
-if not os.path.exists(saved_fig_filepath):
-    os.makedirs(saved_fig_filepath)
 
 # Model directory
 model_dir = os.path.join(os.path.dirname(__file__), 'model')
@@ -48,7 +41,11 @@ AP_model.protocol = modelling.ProtocolLibrary().current_impulse(pulse_time)
 
 Hill_model = modelling.HillsModel()
 
-# TODO: Turn off parallelisation of PINTS default
+init_param_values = pd.DataFrame([0] * 5, index=param_names)
+init_param_values = init_param_values.T
+ComparisonController = modelling.ModelComparison(init_param_values)
+
+
 def model_comparison(param_values):
     orig_param_values = pd.DataFrame(param_values, index=param_names)
     orig_param_values = orig_param_values.T
@@ -58,16 +55,19 @@ def model_comparison(param_values):
     orig_param_values['Ku'] = 10**orig_param_values['Ku']
     orig_param_values['EC50'] = 10**orig_param_values['EC50']
 
-    ComparisonController = modelling.ModelComparison(orig_param_values)
-    Hill_curve_coefs, drug_conc_Hill, _ = \
+    ComparisonController.drug_param_values = orig_param_values
+    Hill_curve_coefs, drug_conc_Hill, peak_norm = \
         ComparisonController.compute_Hill(BKmodel)
 
     drug_conc_range = (np.log10(drug_conc_Hill[1]),
                        np.log10(drug_conc_Hill[-1]))
-    MSError, _, _ = ComparisonController.compute_MSE(
-        AP_model, Hill_curve_coefs, drug_conc=drug_conc_range)
+    try:
+        RMSError, _, _ = ComparisonController.compute_RMSE(
+            AP_model, Hill_curve_coefs, drug_conc=drug_conc_range)
+    except:
+       RMSError = 0 #float("Nan")
 
-    return MSError
+    return RMSError
 
 
 # Define the model inputs
@@ -81,21 +81,17 @@ problem = {
                [0, 10]],
 }
 
-# TODO: Estimate time usage
 # Generate samples
-param_values = saltelli.sample(problem, 4)
+samples_n = 2
+param_values = saltelli.sample(problem, samples_n)
+np.savetxt(os.path.join(saved_data_filepath, "param_value_samples.txt"), param_values)
 
-# Run model (example)
-Y = np.zeros([param_values.shape[0]])
+# Evaluate function
+evaluator = pints.ParallelEvaluator(model_comparison, n_numpy_threads=0)
+output = evaluator.evaluate(param_values)
+Y = np.array(output)
 
-# TODO: parallelise
-for i, X in enumerate(param_values):
-    print('evaluating...')
-    Y[i] = model_comparison(X)
-    print(Y)
-
-# evaluator = pints.ParallelEvaluator(model_comparison)
-# Y = evaluator.evaluate(param_values)
+np.savetxt(os.path.join(saved_data_filepath, "MSError_evaluations.txt"), Y)
 
 # Perform analysis
 Si = sobol.analyze(problem, Y, print_to_console=True, parallel=True,)
@@ -103,6 +99,8 @@ Si = sobol.analyze(problem, Y, print_to_console=True, parallel=True,)
 
 # Save data
 total_Si, first_Si, second_Si = Si.to_df()
+
 total_Si.to_csv(os.path.join(saved_data_filepath, 'total_Si.csv'))
 first_Si.to_csv(os.path.join(saved_data_filepath, 'first_Si.csv'))
 second_Si.to_csv(os.path.join(saved_data_filepath, 'second_Si.csv'))
+

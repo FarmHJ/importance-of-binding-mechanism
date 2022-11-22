20503b9ec0ed08b8bea4e01448d93c17edbabea4
FarmHJ <huijia-farm_95@hotmail.com>
2022-11-22 16:01:55 +0000

    Check Hill curve error with diltiazem P40

--- modelling/scripts/Hill_error_check.py
+++ modelling/scripts/Hill_error_check.py
@@ -17,8 +17,10 @@ model = '../../model/ohara-cipa-v1-2017-IKr.mmt'
 model, _, x = myokit.load(model)
 
 protocol_params = modelling.ProtocolParameters()
-pulse_time = protocol_params.protocol_parameters['Milnes']['pulse_time']
-protocol = protocol_params.protocol_parameters['Milnes']['function']
+# protocol_name = 'Milnes'
+protocol_name = 'P40'
+pulse_time = protocol_params.protocol_parameters[protocol_name]['pulse_time']
+protocol = protocol_params.protocol_parameters[protocol_name]['function']
 
 drug_model = modelling.BindingKinetics(model)
 drug_model.protocol = protocol
@@ -41,10 +43,11 @@ drug_model.protocol = protocol
 # APD_points = 20
 
 param_lib = modelling.BindingParameters()
-drug_list = ['mexiletine']
+# drug_list = ['mexiletine']
+drug_list = ['diltiazem']
 SA_model = modelling.SensitivityAnalysis()
 param_names = SA_model.param_names
-parameter_interest = 'N'
+# parameter_interest = 'N'
 
 starting_param_df = pd.DataFrame([1] * 5, index=param_names).T
 ComparisonController = modelling.ModelComparison(starting_param_df)
@@ -67,31 +70,42 @@ def param_evaluation(param_values):
     return Hill_curve_coefs, drug_conc_Hill, peaks_norm
 
 
-filename = 'SA_mexiletine_N_test.csv'
-result_df = pd.read_csv(saved_data_dir + filename,
-                        header=[0, 1], index_col=[0],
-                        skipinitialspace=True)
+# filename = 'SA_mexiletine_N_test.csv'
+# result_df = pd.read_csv(saved_data_dir + filename,
+#                         header=[0, 1], index_col=[0],
+#                         skipinitialspace=True)
 
-Hill_coefs = result_df['Hill_curve']['Hill_coef'].values
-nan_ind = [i for i in range(len(Hill_coefs)) if np.isnan(Hill_coefs[i])]
+# Hill_coefs = result_df['Hill_curve']['Hill_coef'].values
+# nan_ind = [i for i in range(len(Hill_coefs)) if np.isnan(Hill_coefs[i])]
 
-Vhalf = result_df['param_values']['Vhalf'].values[nan_ind[0]]
-Kmax = result_df['param_values']['Kmax'].values[nan_ind[0]]
-Ku = result_df['param_values']['Ku'].values[nan_ind[0]]
-N = result_df['param_values']['N'].values[nan_ind[0]]
-EC = result_df['param_values']['EC50'].values[nan_ind[0]]
+# Vhalf = result_df['param_values']['Vhalf'].values[nan_ind[0]]
+# Kmax = result_df['param_values']['Kmax'].values[nan_ind[0]]
+# Ku = result_df['param_values']['Ku'].values[nan_ind[0]]
+# N = result_df['param_values']['N'].values[nan_ind[0]]
+# EC = result_df['param_values']['EC50'].values[nan_ind[0]]
+drug = drug_list[0]
+Vhalf = param_lib.binding_parameters[drug]['Vhalf']
+Kmax = param_lib.binding_parameters[drug]['Kmax']
+Ku = param_lib.binding_parameters[drug]['Ku']
+N = param_lib.binding_parameters[drug]['N']
+EC50 = param_lib.binding_parameters[drug]['EC50']
+
 paramid = 0
-param_values = pd.DataFrame([paramid, Vhalf, Kmax, Ku, N, EC],
+param_values = pd.DataFrame([paramid, Vhalf, Kmax, Ku, N, EC50],
                             index=['param_id'] + param_names)
 param_values = param_values.T
 Hill_curve_coefs, drug_conc_Hill, peaks_norm = param_evaluation(param_values)
+
+Hill_model = modelling.HillsModel()
+reduction_scale = Hill_model.simulate(Hill_curve_coefs, drug_conc_Hill)
 
 print(Hill_curve_coefs)
 
 plt.figure()
 plt.plot(drug_conc_Hill, peaks_norm, 'o')
+plt.plot(drug_conc_Hill, reduction_scale, '-')
 plt.xscale('log')
-plt.savefig('../../figures/testing/Hill_error.pdf')
+plt.savefig('../../figures/testing/Hill_error_diltiazem.pdf')
 
 # combined_result_df = pd.concat([combined_result_df, result_df.T])
 
